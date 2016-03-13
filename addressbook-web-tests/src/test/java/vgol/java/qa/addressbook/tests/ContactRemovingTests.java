bb112beec00ac0f556fad69e5ef7bbe36afe162d
Vladimir Golubkov <vgol.mail@gmail.com>
2016-03-13 17:35:26 +0300

    Add checks to contact deletion tests.

--- addressbook-web-tests/src/test/java/vgol/java/qa/addressbook/tests/ContactRemovingTests.java
+++ addressbook-web-tests/src/test/java/vgol/java/qa/addressbook/tests/ContactRemovingTests.java
@@ -1,7 +1,10 @@
 package vgol.java.qa.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import vgol.java.qa.addressbook.model.ContactData;
+
+import java.util.List;
 
 public class ContactRemovingTests extends TestBase {
 
@@ -11,9 +14,16 @@ public class ContactRemovingTests extends TestBase {
     if (! app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createContact(new ContactData("John", "Smith", "somewhere", "john.smith@somewhere.org"));
     }
+    List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().selectContact();
     app.getContactHelper().deleteContact();
     app.getContactHelper().alertAccept();
+    app.getNavigationHelper().gotoHomePage();
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
   }
 
   @Test
@@ -22,8 +32,15 @@ public class ContactRemovingTests extends TestBase {
     if (! app.getContactHelper().isThereAContact()) {
       app.getContactHelper().createContact(new ContactData("John", "Smith", "somewhere", "john.smith@somewhere.org"));
     }
+    List<ContactData> before = app.getContactHelper().getContactList();
     app.getContactHelper().selectContact();
     app.getContactHelper().clickEditOnElement();
     app.getContactHelper().deleteContactFromEdit();
+    app.getNavigationHelper().gotoHomePage();
+    List<ContactData> after = app.getContactHelper().getContactList();
+    Assert.assertEquals(after.size(), before.size() - 1);
+
+    before.remove(before.size() - 1);
+    Assert.assertEquals(before, after);
   }
 }
