a2988b89960bb52cdef19f500c72ead3afb7a4f4
Vladimir Golubkov <vgol.mail@gmail.com>
2016-03-13 13:53:10 +0300

    * change GroupData.id type to int; * add checks to group creation test;

--- addressbook-web-tests/src/test/java/vgol/java/qa/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/vgol/java/qa/addressbook/model/GroupData.java
@@ -1,26 +1,26 @@
 package vgol.java.qa.addressbook.model;
 
 public class GroupData {
-  private final String id;
+  private int id;
   private final String groupname;
   private final String header;
   private final String footer;
 
   public GroupData(String groupname, String header, String footer) {
-    this.id = null;
+    this.id = 0;
     this.groupname = groupname;
     this.header = header;
     this.footer = footer;
   }
 
-  public GroupData(String id, String groupname, String header, String footer) {
+  public GroupData(int id, String groupname, String header, String footer) {
     this.id = id;
     this.groupname = groupname;
     this.header = header;
     this.footer = footer;
   }
 
-  public String getId() {
+  public int getId() {
     return id;
   }
 
@@ -36,11 +36,15 @@ public class GroupData {
     return footer;
   }
 
+  public void setId(int id) {
+    this.id = id;
+  }
+
   @Override
   public String toString() {
     return "GroupData{" +
-        "groupname='" + groupname + '\'' +
-        ", id='" + id + '\'' +
+        "id=" + id +
+        ", groupname='" + groupname + '\'' +
         '}';
   }
 
@@ -51,15 +55,14 @@ public class GroupData {
 
     GroupData groupData = (GroupData) o;
 
-    if (getId() != null ? !getId().equals(groupData.getId()) : groupData.getId() != null)
-      return false;
+    if (getId() != groupData.getId()) return false;
     return getGroupname() != null ? getGroupname().equals(groupData.getGroupname()) : groupData.getGroupname() == null;
 
   }
 
   @Override
   public int hashCode() {
-    int result = getId() != null ? getId().hashCode() : 0;
+    int result = getId();
     result = 31 * result + (getGroupname() != null ? getGroupname().hashCode() : 0);
     return result;
   }
