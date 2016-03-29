3268a08e62312eaa4917ce63d80a6be33bd30a2e
Vladimir Golubkov <vgol.mail@gmail.com>
2016-03-29 21:48:06 +0300

    made some methods private instead of public

--- addressbook-web-tests/src/test/java/vgol/java/qa/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/vgol/java/qa/addressbook/appmanager/GroupHelper.java
@@ -11,41 +11,41 @@ import java.util.List;
 
 public class GroupHelper extends HelperBase {
 
-  public GroupHelper(WebDriver wd) {
+  GroupHelper(WebDriver wd) {
     super(wd);
   }
 
-  public void returnToGroupPage() {
+  private void returnToGroupPage() {
     click(By.linkText("group page"));
   }
 
-  public void submitGroupCreation() {
+  private void submitGroupCreation() {
     click(By.name("submit"));
   }
 
-  public void fillGroupForm(GroupData groupData) {
+  private void fillGroupForm(GroupData groupData) {
     type(By.name("group_name"), groupData.getGroupname());
     type(By.name("group_header"), groupData.getHeader());
     type(By.name("group_footer"), groupData.getFooter());
   }
 
-  public void initCreateGroup() {
+  private void initCreateGroup() {
     click(By.name("new"));
   }
 
-  public void deleteGroup() {
+  private void deleteGroup() {
     click(By.xpath("//div[@id='content']/form/input[5]"));
   }
 
-  public void selectGroup(int index) {
+  private void selectGroup(int index) {
     wd.findElements(By.name("selected[]")).get(index).click();
   }
 
-  public void initModificationGroup() {
+  private void initModificationGroup() {
     click(By.xpath("//div[@id='content']/form/input[6]"));
   }
 
-  public void submitGroupModification() {
+  private void submitGroupModification() {
     click(By.name("update"));
   }
 
--- addressbook-web-tests/src/test/java/vgol/java/qa/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/vgol/java/qa/addressbook/tests/TestBase.java
@@ -7,7 +7,7 @@ import vgol.java.qa.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
   @BeforeSuite
   public void setUp() throws Exception {
