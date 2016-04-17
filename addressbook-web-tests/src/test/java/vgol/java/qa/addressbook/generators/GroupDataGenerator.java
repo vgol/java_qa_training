10e336db11b54e0bbb5d1b08151f69d0c176fa87
Vladimir Golubkov <vgol.mail@gmail.com>
2016-04-17 09:09:16 +0300

    use JCommander for args handling

--- addressbook-web-tests/src/test/java/vgol/java/qa/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/vgol/java/qa/addressbook/generators/GroupDataGenerator.java
@@ -1,6 +1,9 @@
 package vgol.java.qa.addressbook.generators;
 
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import vgol.java.qa.addressbook.model.GroupData;
 
 import java.io.File;
@@ -12,12 +15,28 @@ import java.util.List;
 
 public class GroupDataGenerator {
 
-  public static void main(String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
+  @Parameter(names = "-c", description = "Groups count")
+  public int count;
 
+  @Parameter(names = "-f", description = "Target file")
+  public String file;
+
+
+  public static void main(String[] args) throws IOException {
+    GroupDataGenerator generator = new GroupDataGenerator();
+    JCommander jCommander = new JCommander(generator);
+    try {
+      jCommander.parse(args);
+    } catch (ParameterException exc) {
+      jCommander.usage();
+      return;
+    }
+    generator.run();
+    }
+
+  private void run() throws IOException {
     List<GroupData> groups = generateGroups(count);
-    save(groups, file);
+    save(groups, new File(file));
   }
 
   private static void save(List<GroupData> groups, File file) throws IOException {
@@ -27,6 +46,7 @@ public class GroupDataGenerator {
     }
     writer.close();
   }
+
 
   private static List<GroupData> generateGroups(int count) {
     List<GroupData> groups = new ArrayList<>();
