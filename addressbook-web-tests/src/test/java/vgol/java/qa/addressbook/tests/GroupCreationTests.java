4dc82513dd5b0277d04fee1102ce6a13b9be2b90
Vladimir Golubkov <vgol.mail@gmail.com>
2016-04-17 09:51:43 +0300

    Group creation test parametrized.

--- addressbook-web-tests/src/test/java/vgol/java/qa/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/vgol/java/qa/addressbook/tests/GroupCreationTests.java
@@ -1,19 +1,33 @@
 package vgol.java.qa.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import vgol.java.qa.addressbook.model.GroupData;
 import vgol.java.qa.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() {
+  @DataProvider
+  public Iterator<Object[]> validGroups() {
+    List<Object[]> list = new ArrayList<>();
+    for (int i = 0; i < 3; i++) {
+      list.add(new Object[]{new GroupData().withName("group" + i)
+          .withHeader("header" + i).withFooter("footer0" + i)});
+    }
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("group33").withHeader("text").withFooter("text");
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size() + 1));
     Groups after = app.group().all();
