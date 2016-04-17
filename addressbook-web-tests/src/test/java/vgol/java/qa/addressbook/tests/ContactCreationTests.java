2e921a7ef858b01a45eee0464fcaafd02c9dc5f0
Vladimir Golubkov <vgol.mail@gmail.com>
2016-04-17 12:51:00 +0300

    Use data provider in contacts creation tests.

--- addressbook-web-tests/src/test/java/vgol/java/qa/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/vgol/java/qa/addressbook/tests/ContactCreationTests.java
@@ -1,26 +1,42 @@
 package vgol.java.qa.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import vgol.java.qa.addressbook.model.ContactData;
 import vgol.java.qa.addressbook.model.Contacts;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() {
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/contacts.json"));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null) {
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<ContactData> groups = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
+    return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+}
+
+  @Test(dataProvider = "validContactsFromJson")
+  public void testContactCreation(ContactData contact) {
     app.goTo().homePage();
     Contacts before = app.contact().all();
-    ContactData contact = new ContactData()
-        .withFirstname("John")
-        .withLastname("Smith")
-        .withAddress("somewhere")
-        .withEmail("john.smith@somewhere.org")
-        .withHomePhone("+7 (495) 222 33 44")
-        .withMobilePhone("89263332221")
-        .withWorkPhone("333-66-88");
     app.contact().create(contact);
     Contacts after = app.contact().all();
     assertThat(after.size(), equalTo(before.size() + 1));
