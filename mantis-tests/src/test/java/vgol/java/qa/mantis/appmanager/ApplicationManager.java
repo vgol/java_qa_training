e450ce49ff29af39133ff70853f474addd32d46a
Vladimir Golubkov <vgol.mail@gmail.com>
2016-04-24 18:16:51 +0300

    Implement lazy initialization;

--- mantis-tests/src/test/java/vgol/java/qa/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/vgol/java/qa/mantis/appmanager/ApplicationManager.java
@@ -17,8 +17,8 @@ public class ApplicationManager {
 
   private final Properties properties;
   private WebDriver wd;
-
   private String browser;
+  private RegistrationHelper registrationHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -28,27 +28,12 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new  File(String.format("src/test/resources/%s.properties", target))));
-
-    switch (browser) {
-      case BrowserType.FIREFOX:
-        wd = new FirefoxDriver();
-        break;
-      case BrowserType.CHROME:
-        wd = new ChromeDriver();
-        break;
-      case BrowserType.EDGE:
-        // Driver is not stable yet. There may be problems.
-        File edgewd = new File(properties.getProperty("driver.webDriverEdge"));
-        System.setProperty("webdriver.edge.driver", edgewd.getAbsolutePath());
-        wd = new EdgeDriver();
-        break;
-    }
-    wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    wd.get(properties.getProperty("web.baseUrl"));
   }
 
   public void stop() {
-    wd.quit();
+    if (wd != null) {
+      wd.quit();
+    }
   }
 
   public HttpSession newSession() {
@@ -57,6 +42,36 @@ public class ApplicationManager {
 
   public String getProperty(String key) {
     return properties.getProperty(key);
+  }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper == null) {
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+
+  public WebDriver getDriver() {
+    if (wd == null) {
+      switch (browser) {
+        case BrowserType.FIREFOX:
+          wd = new FirefoxDriver();
+          break;
+        case BrowserType.CHROME:
+          wd = new ChromeDriver();
+          break;
+        case BrowserType.EDGE:
+          // Driver is not stable yet. There may be problems.
+          File edgewd = new File(properties.getProperty("driver.webDriverEdge"));
+          System.setProperty("webdriver.edge.driver", edgewd.getAbsolutePath());
+          wd = new EdgeDriver();
+          break;
+      }
+      wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+      wd.get(properties.getProperty("web.baseUrl"));
+
+    }
+    return wd;
   }
 }
 
